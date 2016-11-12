5d8bdcc4f16a060480776879c9b280c796663369
ajlarkins <ajlarkins@csu.fullerton.edu>
2016-11-11 22:44:27 -0800

    Update ChutesAndLaddersGame.cpp

--- ChutesAndLaddersGame.cpp
+++ ChutesAndLaddersGame.cpp
@@ -11,30 +11,53 @@
 
 using namespace std;
 
-// TODO: implement the constructor with all your team members
 // constructor with the default value of a minimum players
 ChutesAndLaddersGame::ChutesAndLaddersGame(int nPlayers) : winner("no winner") {
-   // TODO: implement this function properly
-   throw std::logic_error("not implemented yet");
+   // CREATE PLAYER: ALEC
+   Player Alec;
+   Alec.setName("Alec");
+   Q.enqueue(Alec);
+   
+   // CREATE PLAYER: GHOST
+   Player Ghost;
+   Ghost.setName("Ghost");
+   Q.enqueue(Ghost);
 }
 
-// TODO: implement the destructor
-// destructor - dequeue players from the queue
+
+// DESTRUCTOR: DEQUEUE PLAYERS FROM QUEUE
 ChutesAndLaddersGame::~ChutesAndLaddersGame() {
-   // TODO: implement this function properly
-   throw std::logic_error("not implemented yet");
+   while (!Q.empty())
+   {
+      Q.dequeue();
+   }
 }
 
-// TO DO: implement this function properly
 // reset the game - rebuild the list of players
 //        (i.e., the list should be the same as in the constructor).
 //        Place all players at the figurative square zero
+
+// RESET GAME
 void ChutesAndLaddersGame::resetGame() {
-   // TODO: implement this function properly
-   throw std::logic_error("not implemented yet");
+   // DEQUEUE LIST OF PLAYERS
+   while (!Q.empty())
+   {
+      Q.dequeue();
+   }
+   
+   // REBUILD LIST OF PLAYERS
+   Player Alec;
+   Alec.setName("Alec");
+   Alec.setPosition(0);
+   Q.enqueue(Alec);
+   
+   Player Ghost;
+   Ghost.setName("Ghost");
+   Ghost.setPosition(0);
+   Q.enqueue(Ghost);
 }
 
-// TO DO: implement this function properly
+
 // Play the chutes and ladders until a player reaches the winning square 100
 //    - Each player takes turn to roll the die and moves to the new square by invoking rollDieAndMove.
 //         If the new square is outside of the board, the player stays put
@@ -44,8 +67,21 @@ void ChutesAndLaddersGame::resetGame() {
 //          If player lands on a chute or a ladder, player slides down or climbs up
 //    - If player lands on the winning square 100, game is over
 //    - playGame returns after congratulating and printing the winner's name
+
+// PLAY GAME FUNCTION
 void ChutesAndLaddersGame::playGame() {
-   // TODO: implement this function properly
-   throw std::logic_error("not implemented yet");
-   
+   winner = "NONE";
+   while(winner == "NONE")
+   {
+      Player T = Q.front();
+      int tempValue;
+      tempValue = T.rollDieAndMove();
+      gameBoard.checkChutesLadders()
+      
+      if (T.getPosition() == WINNING_POSITION)
+      {
+         cout << "Congratulations " << T.getName() << ", you won!" << endl;
+         winner = T.getName();
+      }
+   }
 }
